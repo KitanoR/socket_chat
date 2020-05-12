2a8ea67e7fc31f38da8e6b7fb19d359b894bbfcc
Cayetano Rosales <cayeroza@gmail.com>
2020-05-11 21:53:39 -0600

    Socket terminado, chat terminado

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -19,6 +19,7 @@ socket.on('connect', function() {
 
     socket.emit('entrarChat', usuario, function(resp) {
         console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
 
 });
@@ -42,12 +43,15 @@ socket.on('disconnect', function() {
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
     console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
 // Escuchar cambios de usuarios
 // cuando un usuario entra o sale del chat
-socket.on('listaPersona', function(personas) {
+socket.on('listaPersonas', function(personas) {
     console.log(personas);
+    renderizarUsuarios(personas);
 });
 
 // Mensajes privados
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -19,10 +19,12 @@ io.on('connection', (client) => {
         client.broadcast.to(usuario.sala).emit('listaPersonas', usuarios.getPersonasPorSala(usuario.sala));
         callback(usuarios.getPersonasPorSala(usuario.sala));
     });
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
         let persona = usuarios.getPersona(client.id)
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
     });
 
     client.on('disconnect', () => {
